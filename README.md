94b1406a0026b102874465a964afe8dc86ece1d5
Nicolas Padula <nahog@msn.com>
2018-06-14 21:56:33 +0100

    Implement football-data and coperos as modules

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ A bot for slack to message about matches of the 2018 FIFA World Cup
 ## What you have to do
 - Configure the slack webhook https://api.slack.com/incoming-webhooks
 - Get an api token from https://api.football-data.org/client/register
-- Run the app somewhere (node index.js)
+- Run the app somewhere (node index.js football-data)
+
+## Modules (NEW!)
+Now you can develop your own modules to make the bot do other things based on actions. The football-data is now a module, so starting the app must include the module. As an example there is another module now, that parse the web www.coperos.com for a rooster and post the rooster to the channel. To launch the new module the command is (node index.js coperos).
+For example you can run the football-data every 15 minutes and the coperos once a day
+```
+CRON_SCHEDULE="*/10 * * * *" node index.js football-data
+CRON_SCHEDULE="0 23 * * *" node index.js coperos
+```
 
 ## What it does for you
 - Post every day, the matches of the day
@@ -17,15 +25,23 @@ A bot for slack to message about matches of the 2018 FIFA World Cup
 
 ## Configs
 All configurations can be modified by environmental variables, here are the different configurations available:
-- LANGUAGE - type: string, default: `"es"`, Language for the translations, the "en" language is already included, any new language should be added as a new file in the locales folder following the convention of the other files already there.
-- HIGHLIGHTED_TEAM - type: string, default: `"Argentina"`, A team to highligh (bold) in the posted message, it should match the team name that comes from the Api.
-- SHOW_ZONES_JSON, type: json, default: `'{ "ART": "-3", "IST": "+1" }'`, A key-value object that represent the available timezones where the match time will be shown, the key could be anything (preferably a timezone name), the value should be the integer offset hours of the timezone.
+
+### General
+- LOG_LEVEL - type: string, default `"debug"`, The log level to the console (error, info, debug, silly, etc...)
+- LANGUAGE - type: string, default: `"en"`, Language for the translations, the "es" language is already included, any new language should be added as a new file in the locales folder following the convention of the other files already there.
 - CRON_SCHEDULE, type: string (cron expression), default: `"*/10 * * * *"`, The cron expression to schedule each Api check, see https://es.wikipedia.org/wiki/Cron_(Unix)
 - SLACK_ENABLED, type: boolean, default: `true`, Enable or disable the post to slack, good for testing the configuration.
 - SLACK_CHANNEL, type: string, default: `"#worldcup"`, The name of the slack channel that the bot will post to.
 - SLACK_BOT, type: string, default: `"Worldcup"`, The name of the bot (configured in slack) that will make the post.
 - SLACK_WEBHOOK, type string, default: `"https://hooks.slack.com/services/***`, The slack webhook url, the default will not work, you need to get this from slack when creating the webhook.
+
+### Football-data module
+- HIGHLIGHTED_TEAM - type: string, default: `"Argentina"`, A team to highligh (bold) in the posted message, it should match the team name that comes from the Api.
+- SHOW_ZONES_JSON, type: json, default: `'{ "ART": "-3", "IST": "+1" }'`, A key-value object that represent the available timezones where the match time will be shown, the key could be anything (preferably a timezone name), the value should be the integer offset hours of the timezone.
 - FOOTBALL_DATA_API_TOKEN, type string, default: `"***"`, The Api token used to get the match data, the default will not work, you need to get this from the Api page.
+
+### Coperos module
+- COPEROS_TOURNAMENT, type string, default: `"rusia_2018_1.html"`, The url of the www.coperos.com tournament to get the rooster.
 
 ## Sample post
 `Nueva actualizacion para Rusia :flag-ru: vs :flag-sa: Arabia Saudita, Rusia :flag-ru: 5 - 0 :flag-sa: Arabia Saudita`
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const translations = require("translations");
-const flagsEmoji = require("./flags.json")
 const schedule = require("node-schedule");
 const Slack = require("slack-node");
 const winston = require("winston");
-const moment = require("moment");
-const http = require("http");
-const _ = require("lodash");
 const fs = require("fs");
 
 const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || "info",
+    level: process.env.LOG_LEVEL || "debug",
     format: winston.format.cli(),
     transports: [
         new winston.transports.File({
@@ -21,146 +17,28 @@ const logger = winston.createLogger({
 });
 logger.info("proccess started");
 
-const language = process.env.LANGUAGE || "es"; // The "en" language is already implemented, for others you need to create a new file in the locales folder
+const language = process.env.LANGUAGE || "en"; // The "es" language is already implemented, for others you need to create a new file in the locales folder
 const locale = require("./locales/" + language + ".json");
 const t = translations(locale);
-const highlightedTeam = process.env.HIGHLIGHTED_TEAM || "Argentina"; // It should match the team name that comes from the Api
-const showZonesJson = process.env.SHOW_ZONES_JSON || '{ "ART": "-3", "IST": "+1" }';
-const showZones = JSON.parse(showZonesJson);
-const cronSchedule = process.env.CRON_SCHEDULE || "*/10 * * * *";
-const slackEnabled = process.env.SLACK_ENABLED || true;
+const cronSchedule = process.env.CRON_SCHEDULE || "*/10 * * * * *";
+const slackEnabled = process.env.SLACK_ENABLED || false;
 const slackChannel = process.env.SLACK_CHANNEL || "#worldcup";
 const slackBot = process.env.SLACK_BOT || "Worldcup";
 const slackWebhook = process.env.SLACK_WEBHOOK || "https://hooks.slack.com/services/***";
-const footballDataApiToken = process.env.FOOTBALL_DATA_API_TOKEN || "***";
 const slack = new Slack();
-const dbFile = "db.json";
-const dbCleanFile = "db.clean.json";
-const dataApiOptions = {
-    host: "api.football-data.org",
-    port: 80,
-    path: "/v1/competitions/467/fixtures",
-    method: "GET",
-    headers: {
-        "X-Auth-Token": footballDataApiToken
-    }
-};
 
 logger.debug(
-    "the current cron schedule for checking results is: " + cronSchedule
+    "the current cron schedule for calling the action is: " + cronSchedule
 );
 
 slack.setWebhook(slackWebhook);
-
-if (!fs.existsSync(dbFile)) {
-    logger.info(
-        "db file does not exists, creating one from the clean db template"
-    );
-    fs.copyFileSync(dbCleanFile, dbFile);
-}
-
+var action = require("./actions/" + process.argv[2]);
 let job = schedule.scheduleJob(cronSchedule, runJob);
 
 function runJob() {
-    const today = getToday();
-    logger.info("schedule fired on: " + today.toISOString());
-
-    http
-        .request(dataApiOptions, res => {
-            logger.info("api call made, response: " + res.statusCode);
-            logger.log("silly", "api call made, headers: " + JSON.stringify(res.headers));
-            res.setEncoding("utf8");
-            let body = "";
-            res.on("data", chunk => {
-                logger.debug("data from api call arrived");
-                logger.log("silly", "data from api call arrived, chunk: " + chunk);
-                body += chunk;
-            });
-            res.on("end", () => {
-                logger.debug("data from api call completed");
-                logger.log("silly", "data from api call arrived, body: " + body);
-
-                let apiData = parseApiData(body);
-
-                let dbData = JSON.parse(fs.readFileSync(dbFile));
-                _.forEach(apiData, apiFixture => {
-                    let dbFixture = _.findLast(dbData, ["id", apiFixture.id]);
-                    processFixture(today, apiFixture, dbFixture);
-                });
-
-                const dbDataAsString = JSON.stringify(dbData, null, 4);
-                logger.log("silly", "data saved to db, dbData: " + dbDataAsString);
-                fs.writeFileSync(dbFile, dbDataAsString);
-                logger.info("api call process ended");
-            });
-            res.on("error", error => {
-                logger.error(error.message);
-            });
-        })
-        .end();
-
+    logger.info("schedule fired on: " + new Date());
+    action(logger, t, postToSlack);
     logger.info("next schedule event at: " + job.nextInvocation());
-}
-
-function processFixture(today, apiFixture, dbFixture) {
-    logger.log("silly", "processing match: " + apiFixture.id);
-
-    const homeTeamDecoration = apiFixture.homeTeamName === highlightedTeam ? "*" : "";
-    const awayTeamDecoration = apiFixture.awayTeamName === highlightedTeam ? "*" : "";
-
-    const fixtureDate = moment(apiFixture.date);
-
-    if (
-        today.date() === fixtureDate.date() &&
-        today.month() === fixtureDate.month() &&
-        today.year() === fixtureDate.year()
-    ) {
-        if (!dbFixture.posted) {
-            const matchHour = getMatchHour(fixtureDate);
-            if (apiFixture.goalsHomeTeam === null ||
-                apiFixture.goalsAwayTeam === null) {
-                postToSlack(t("Today's match {home} vs {away} at {date}", {
-                    home: homeTeamDecoration + t(apiFixture.homeTeamName) + homeTeamDecoration + " " + flagsEmoji[apiFixture.homeTeamName],
-                    away: flagsEmoji[apiFixture.awayTeamName] + " " + awayTeamDecoration + t(apiFixture.awayTeamName) + awayTeamDecoration,
-                    date: matchHour
-                }));
-            } else {
-                postToSlack(t("Today's match {home} vs {away} at {date}, current score {home} {homeGoals} - {awayGoals} {away}", {
-                    home: homeTeamDecoration + t(apiFixture.homeTeamName) + homeTeamDecoration + " " + flagsEmoji[apiFixture.homeTeamName],
-                    away: flagsEmoji[apiFixture.awayTeamName] + " " + awayTeamDecoration + t(apiFixture.awayTeamName) + awayTeamDecoration,
-                    homeGoals: apiFixture.goalsHomeTeam,
-                    awayGoals: apiFixture.goalsAwayTeam,
-                    date: matchHour
-                }));
-            }
-        } else {
-            if (
-                apiFixture.goalsHomeTeam !== dbFixture.goalsHomeTeam ||
-                apiFixture.goalsAwayTeam !== dbFixture.goalsAwayTeam
-            ) {
-                postToSlack(t("New update for {home} vs {away}, {home} {homeGoals} - {awayGoals} {away}", {
-                    home: homeTeamDecoration + t(apiFixture.homeTeamName) + homeTeamDecoration + " " + flagsEmoji[apiFixture.homeTeamName],
-                    away: flagsEmoji[apiFixture.awayTeamName] + " " + awayTeamDecoration + t(apiFixture.awayTeamName) + awayTeamDecoration,
-                    homeGoals: apiFixture.goalsHomeTeam,
-                    awayGoals: apiFixture.goalsAwayTeam
-                }));
-            }
-        }
-
-        dbFixture.goalsHomeTeam = apiFixture.goalsHomeTeam;
-        dbFixture.goalsAwayTeam = apiFixture.goalsAwayTeam;
-        dbFixture.posted = true;
-    }
-}
-
-function getMatchHour(date) {
-    let hours = [];
-    for (var zone in showZones) {
-        if (showZones.hasOwnProperty(zone)) {
-            hours.push(date.clone().utc().add(parseInt(showZones[zone]), "hours").format("LT") + " " + zone);
-        }
-    }
-    return hours.join(", ");
 }
 
 function postToSlack(message) {
@@ -183,40 +61,6 @@ function postToSlack(message) {
     } else {
         logger.info("slack is not enabled, not posting");
     }
-}
-
-function parseApiData(body) {
-    let apiData = [];
-    const bodyData = JSON.parse(body);
-    if (!bodyData || bodyData.error) {
-        logger.error(bodyData.error);
-        return apiData;
-    }
-    _.forEach(bodyData.fixtures, fixture => {
-        apiData.push({
-            id: getId(fixture),
-            posted: false,
-            date: fixture.date,
-            homeTeamName: fixture.homeTeamName,
-            awayTeamName: fixture.awayTeamName,
-            goalsHomeTeam: fixture.result.goalsHomeTeam,
-            goalsAwayTeam: fixture.result.goalsAwayTeam
-        });
-    });
-    return apiData;
-}
-
-function getId(fixture) {
-    // After this date there is only one match per day and we dont know the
-    // teams yet, so the id is just the date
-    if (moment(fixture.date).isAfter("2018-06-30T00:00:00Z")) {
-        return fixture.date;
-    }
-    return fixture.date + "_" + fixture.homeTeamName + "_" + fixture.awayTeamName;
-}
-
-function getToday() {
-    return moment();
 }
 
 process.on("uncaughtException", error => {
